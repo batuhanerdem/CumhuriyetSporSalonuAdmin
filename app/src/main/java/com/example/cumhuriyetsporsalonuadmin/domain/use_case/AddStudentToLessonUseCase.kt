e6013540303eeed438a8811186669dae3addb281
Batuhan <batuhanerdem173@gmail.com>
2024-07-20 21:29:32 +0300

    AddStudentToLessonUseCase has the solution

--- app/src/main/java/com/example/cumhuriyetsporsalonuadmin/domain/use_case/AddStudentToLessonUseCase.kt
+++ app/src/main/java/com/example/cumhuriyetsporsalonuadmin/domain/use_case/AddStudentToLessonUseCase.kt
@@ -15,8 +15,10 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,36 +32,15 @@ class AddStudentToLessonUseCase @Inject constructor(private val repository: Fire
                 addAll(studentList.map { it.uid })
             }
             lesson.studentUids = newList
-            val lessonResunt = repository.setLesson(lesson)
-            lessonResunt.collect { result ->
+            val lessonResult = repository.setLesson(lesson)
+            lessonResult.collect { result ->
                 when (result) {
                     is Resource.Success -> {
-                        studentList.forEach { student ->
-                            Log.d("tag", "student: $student, $studentList")
-                            val a = CoroutineScope(Dispatchers.IO).async {
-                                addLessonUidToStudent(
-                                    lesson.uid, student.uid
-                                ).collect { studentResult ->
-                                    Log.d(
-                                        "tag",
-                                        "execute: $studentResult ${studentResult.data} - ${studentResult.message}"
-                                    )
-                                    if (studentResult is Resource.Error) emit(studentResult)
-                                }
-                            }
-                            a.await() // olmuslerini sikiyim 
-//                            addLessonUidToStudent2(
-//                                lesson.uid, student.uid
-//                            )
-//                                .collect { studentResult ->
-//                                Log.d(
-//                                    "tag",
-//                                    "execute: $studentResult ${studentResult.data} - ${studentResult.message}"
-//                                )
-//                                if (studentResult is Resource.Error) emit(studentResult)
-//                            }
+                        studentList.asFlow().flatMapMerge { student ->
+                            addLessonUidToStudent(lesson.uid, student.uid)
+                        }.collect {
+                            emit(it)
                         }
-                        emit(Resource.Success())
                     }
 
                     else -> emit(result)
@@ -92,27 +73,4 @@ class AddStudentToLessonUseCase @Inject constructor(private val repository: Fire
         }
     }
 
-    private suspend fun addLessonUidToStudent2(
-        lessonUid: String, studentUid: String
-    ) {
-        repository.getStudentByUid(studentUid).collect { result ->
-            Log.d("tag", "addLessonUidToStudent: $result ${result.data} - ${result.message}")
-            when (result) {
-                is Resource.Success -> {
-                    val student = result.data ?: return@collect
-                    val newList = student.lessonUids.toMutableList().apply {
-                        add(lessonUid)
-                    }
-                    student.lessonUids = newList
-                    repository.setStudent(student).collect {
-//                        if (it is Resource.Error) {
-                        Log.d("tag", "setStudent: $it ${it.data} ${it.message} ")
-//                        }
-                    }
-                }
-
-                else -> {}
-            }
-        }
-    }
 }
